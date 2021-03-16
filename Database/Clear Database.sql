20f63af5529d01e9272d35e0431c15cdd6129bfa
MOPD4H <35518110+zodiacddos@users.noreply.github.com>
2021-03-16 22:26:06 +0800

    Add files via upload

---
+++ Database/Clear Database.sql
@@ -0,0 +1,37 @@
+use [MuOnline]
+
+delete from AccountCharacter
+delete from Achievements
+delete from CashLog
+delete from CashShopData
+delete from CashShopInventory
+delete from CashShopPeriodicItem
+delete from Character
+delete from ExtWarehouse
+delete from Gens_Rank
+delete from Gens_Reward
+delete from Guild
+delete from GuildMember
+delete from HelperData
+delete from ItemLog
+delete from LuckyCoin
+delete from LuckyItem
+delete from MasterSkillTree
+delete from MEMB_INFO
+delete from MEMB_STAT
+delete from OptionData
+delete from QuestKillCount
+delete from QuestWorld
+delete from RankingBloodCastle
+delete from RankingChaosCastle
+delete from RankingDevilSquare
+delete from RankingDuel
+delete from RankingIllusionTemple
+delete from T_CGuid
+delete from T_FriendMail
+delete from T_FriendMain
+delete from T_PetItem_Info
+delete from T_WaitFriend
+delete from warehouse
+delete from WZ_CW_INFO
+update GameServerInfo set ItemCount = 0
